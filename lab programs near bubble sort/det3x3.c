2fb889ffa7030be239c025f8a856c17fa819e29e
harsh <harshsoni13112003@gmail.com>
2023-03-02 00:47:36 +0530

    m

--- lab programs near bubble sort/det3x3.c
+++ lab programs near bubble sort/det3x3.c
@@ -1,3 +1,27 @@
-//
-// Created by harsh on 25/2/23.
-//
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    printf("Enter the dimensions of matrix: ");
+    scanf("%d %d", &x, &y);
+    int arr[x][y];
+    printf("\nInput the array\n");
+    for(int i=0; i<x; i++){
+        for(int j=0; j<y; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("\nYour input array\n");
+    for(int i=0; i<x; i++){
+        for(int j=0; j<y; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    int det;
+    det=
+            arr[0][0]*(arr[1][1]*arr[2][2]-arr[1][2]*arr[2][1])-
+            arr[0][1]*(arr[1][0]*arr[2][2]-arr[2][0]*arr[1][2])+
+            arr[0][2]*(arr[1][0]*arr[2][1]-arr[2][0]*arr[1][1]);
+    printf("Determinant is %d", det);
+}
